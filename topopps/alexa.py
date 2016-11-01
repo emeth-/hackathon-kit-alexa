2c3d5c97a7f43c762143d66362e90a96bce93241
emeth- <seanybob@gmail.com>
2016-11-01 15:36:54 -0500

    should have pushed this

--- topopps/alexa.py
+++ topopps/alexa.py
@@ -89,6 +89,50 @@ def MimicMe(session, house):
     return ResponseBuilder.create_response(**kwargs)
 
 @intent(app="topopps")
+def DetailOpportunity(session, oppname):
+    """
+    Get details for the specified opp name
+    ---
+    details on {oppname}
+    details for {oppname}
+    more info for {oppname}
+    more information for {oppname}
+    """
+    print "***DetailOpportunity session", dict(session)
+    kwargs = {}
+    kwargs['launched'] = launched = session.get('launched')
+    kwargs['oppname'] = oppname = oppname or session.get('oppname')
+    if launched:
+        kwargs['reprompt'] = "What opportunity would you like details for?"
+        kwargs['end_session'] = False
+    kwargs['message'] = str(oppname)+"'s next meeting is on December 12, 2012, and the next steps are call Jim and get the deal signed."
+    kwargs.pop("oppname")
+    return ResponseBuilder.create_response(**kwargs)
+
+@intent(app="topopps")
+def UpdateOpportunityCloseDate(session, closedate, oppname):
+    """
+    update close date for a specified opportunity
+    ---
+    update close date to {closedate} for {oppname}
+    set close date to {closedate} for {oppname}
+    for {oppname} update close date to {closedate}
+    for {oppname} set close date to {closedate}
+    """
+    print "***DetailOpportunity", dict(session), closedate, oppname
+    kwargs = {}
+    kwargs['launched'] = launched = session.get('launched')
+    kwargs['oppname'] = oppname = oppname or session.get('oppname')
+    kwargs['closedate'] = oppname = oppname or session.get('closedate')
+    if launched:
+        kwargs['reprompt'] = "What opportunity would you like to update?"
+        kwargs['end_session'] = False
+    kwargs['message'] = str(oppname)+"'s close date has been updated to "+str(closedate)
+    kwargs.pop("oppname")
+    kwargs.pop("closedate")
+    return ResponseBuilder.create_response(**kwargs)
+
+@intent(app="topopps")
 def BestOpportunities(session):
     """
     The 3 best opportunities
