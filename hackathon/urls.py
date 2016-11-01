d6f460e4546875f915eecbaac866d232bf3e12b4
emeth- <seanybob@gmail.com>
2016-11-01 07:17:15 -0500

    alexa integrated

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     # url(r'^$', 'hackathon.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('django_alexa.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('api.urls')),
+    url(r'^api/', include('api.urls')),
 ]
 
 
